a17f4b315075d5f5ef14fd76d76c05ce8356bf2a
Samuel Lewis <samuelewis@gmail.com>
2012-10-25 10:56:45 -0400

    Start of the c# project

---
+++ asql_assign_4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace asql_assign_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.gb_source = new System.Windows.Forms.GroupBox();
+            this.tb_source = new System.Windows.Forms.TextBox();
+            this.tb_source_info = new System.Windows.Forms.TextBox();
+            this.gb_source.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gb_source
+            // 
+            this.gb_source.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.gb_source.Controls.Add(this.tb_source_info);
+            this.gb_source.Controls.Add(this.tb_source);
+            this.gb_source.Location = new System.Drawing.Point(13, 43);
+            this.gb_source.Name = "gb_source";
+            this.gb_source.Size = new System.Drawing.Size(311, 449);
+            this.gb_source.TabIndex = 1;
+            this.gb_source.TabStop = false;
+            this.gb_source.Text = "Source";
+            // 
+            // tb_source
+            // 
+            this.tb_source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_source.Location = new System.Drawing.Point(7, 20);
+            this.tb_source.Name = "tb_source";
+            this.tb_source.Size = new System.Drawing.Size(298, 20);
+            this.tb_source.TabIndex = 0;
+            this.tb_source.Text = "Data Source=SAMUEL-LAPTOP;Initial Catalog=AdventureWorks2008R2;Integrated Securit" +
+                "y=True";
+            // 
+            // tb_source_info
+            // 
+            this.tb_source_info.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_source_info.Location = new System.Drawing.Point(7, 47);
+            this.tb_source_info.Multiline = true;
+            this.tb_source_info.Name = "tb_source_info";
+            this.tb_source_info.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_source_info.Size = new System.Drawing.Size(298, 396);
+            this.tb_source_info.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 504);
+            this.Controls.Add(this.gb_source);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gb_source.ResumeLayout(false);
+            this.gb_source.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox gb_source;
+        private System.Windows.Forms.TextBox tb_source_info;
+        private System.Windows.Forms.TextBox tb_source;
+    }
+}
+
---
+++ asql_assign_4/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace asql_assign_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_source_info.Clear();
+            SqlConnection db = new SqlConnection(tb_source.Text);
+            db.Open();
+            var tables = db.GetSchema("Tables").AsEnumerable().Select(t => t).OrderBy(t => t["table_name"].ToString().ToLower()).OrderBy(t => t["table_schema"].ToString().ToLower());
+            var columns = db.GetSchema("Columns").AsEnumerable().Select(c => c);
+           
+            foreach (var t in tables )
+            {
+                String table = t["table_schema"] + "." + t["Table_name"];
+                tb_source_info.Text += table + Environment.NewLine;
+                var tableColumns = columns.Where(c => c["table_schema"].Equals(t["table_schema"])).Where(c => c["table_name"].Equals(t["table_name"])).OrderBy(c => c["column_name"].ToString().ToLower());
+
+                foreach (var column in tableColumns)
+                {
+                    tb_source_info.Text += "└" + column["column_name"] + " [" + column["data_type"] + "(" + column["character_maximum_length"] + ")]" + Environment.NewLine;
+                }
+            }
+
+        }
+    }
+}
