57a18c6e587dd93720b9d267ca01a4db429748c1
Samuel Lewis <samuelewis@gmail.com>
2012-11-01 21:09:45 -0400

    Workingish assignment

--- asql_assign_4/Form1.Designer.cs
+++ asql_assign_4/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.tb_dest = new System.Windows.Forms.TextBox();
             this.tb_todo = new System.Windows.Forms.TextBox();
             this.btn_copy = new System.Windows.Forms.Button();
+            this.pb_progress = new System.Windows.Forms.ProgressBar();
             this.gb_source.SuspendLayout();
             this.gb_dest.SuspendLayout();
             this.SuspendLayout();
@@ -51,13 +52,12 @@
             // 
             // gb_source
             // 
-            this.gb_source.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
+            this.gb_source.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.gb_source.Controls.Add(this.tb_source);
             this.gb_source.Location = new System.Drawing.Point(13, 43);
             this.gb_source.Name = "gb_source";
-            this.gb_source.Size = new System.Drawing.Size(311, 51);
+            this.gb_source.Size = new System.Drawing.Size(604, 51);
             this.gb_source.TabIndex = 1;
             this.gb_source.TabStop = false;
             this.gb_source.Text = "Source";
@@ -68,20 +68,18 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.tb_source.Location = new System.Drawing.Point(7, 20);
             this.tb_source.Name = "tb_source";
-            this.tb_source.Size = new System.Drawing.Size(298, 20);
+            this.tb_source.Size = new System.Drawing.Size(591, 20);
             this.tb_source.TabIndex = 0;
-            this.tb_source.Text = "Data Source=SAMUEL-LAPTOP;Initial Catalog=source;Integrated Security=True";
             this.tb_source.TextChanged += new System.EventHandler(this.tb_source_TextChanged);
             // 
             // gb_dest
             // 
-            this.gb_dest.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
+            this.gb_dest.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.gb_dest.Controls.Add(this.tb_dest);
             this.gb_dest.Location = new System.Drawing.Point(13, 100);
             this.gb_dest.Name = "gb_dest";
-            this.gb_dest.Size = new System.Drawing.Size(311, 51);
+            this.gb_dest.Size = new System.Drawing.Size(604, 51);
             this.gb_dest.TabIndex = 2;
             this.gb_dest.TabStop = false;
             this.gb_dest.Text = "Destination";
@@ -92,18 +90,20 @@
                         | System.Windows.Forms.AnchorStyles.Right)));
             this.tb_dest.Location = new System.Drawing.Point(7, 20);
             this.tb_dest.Name = "tb_dest";
-            this.tb_dest.Size = new System.Drawing.Size(298, 20);
+            this.tb_dest.Size = new System.Drawing.Size(591, 20);
             this.tb_dest.TabIndex = 0;
-            this.tb_dest.Text = "Data Source=SAMUEL-LAPTOP;Initial Catalog=tables_exist;Integrated Security=True";
             this.tb_dest.TextChanged += new System.EventHandler(this.tb_dest_TextChanged);
             // 
             // tb_todo
             // 
-            this.tb_todo.Enabled = false;
+            this.tb_todo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
             this.tb_todo.Location = new System.Drawing.Point(13, 158);
             this.tb_todo.Multiline = true;
             this.tb_todo.Name = "tb_todo";
-            this.tb_todo.Size = new System.Drawing.Size(311, 344);
+            this.tb_todo.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tb_todo.Size = new System.Drawing.Size(604, 334);
             this.tb_todo.TabIndex = 3;
             // 
             // btn_copy
@@ -117,18 +117,29 @@
             this.btn_copy.UseVisualStyleBackColor = true;
             this.btn_copy.Click += new System.EventHandler(this.btn_copy_Click);
             // 
+            // pb_progress
+            // 
+            this.pb_progress.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pb_progress.Location = new System.Drawing.Point(177, 12);
+            this.pb_progress.Name = "pb_progress";
+            this.pb_progress.Size = new System.Drawing.Size(440, 23);
+            this.pb_progress.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.pb_progress.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(629, 504);
+            this.Controls.Add(this.pb_progress);
             this.Controls.Add(this.btn_copy);
             this.Controls.Add(this.tb_todo);
             this.Controls.Add(this.gb_dest);
             this.Controls.Add(this.gb_source);
             this.Controls.Add(this.btn_connect);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Table Copier";
             this.gb_source.ResumeLayout(false);
             this.gb_source.PerformLayout();
             this.gb_dest.ResumeLayout(false);
@@ -147,6 +158,7 @@
         private System.Windows.Forms.TextBox tb_dest;
         private System.Windows.Forms.TextBox tb_todo;
         private System.Windows.Forms.Button btn_copy;
+        private System.Windows.Forms.ProgressBar pb_progress;
     }
 }
 
--- asql_assign_4/Form1.cs
+++ asql_assign_4/Form1.cs
@@ -7,86 +7,122 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data.Sql;
+using System.Text.RegularExpressions;
 
 namespace asql_assign_4
 {
     public partial class Form1 : Form
     {
-        List<String> sourceOnly;
-        List<String> both;
+        private Dictionary<String, List<Column>> sources = new Dictionary<string, List<Column>>();
+        List<String> sourceOnly = new List<string>();
+        List<String> both = new List<string>();
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1" /> class.
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Handles the Click event of the button1 control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void button1_Click(object sender, EventArgs e)
         {
-            tb_todo.Clear();
-            btn_copy.Enabled = false;
-            Dictionary<String, List<Row>> source = LoadDB(tb_source.Text);
-            Dictionary<String, List<Row>> dest = LoadDB(tb_dest.Text);
-            List<String> match = new List<string>();
-            List<String> mismatch = new List<string>();
-
-            var source_only = source.Keys.Select(t => t).Where(t => !dest.ContainsKey(t));
-            if(source_only.Count() != 0)
+            try
             {
-                tb_todo.Text += "Source only" + Environment.NewLine;
-                foreach (var s in source_only)
-                {
-                    tb_todo.Text += "└" + s + Environment.NewLine;
-                }
-            }
 
-            var both = source.Select(t => t.Key).Where(t => dest.ContainsKey(t));
-            if (both.Count() != 0)
-            {
-                foreach (var b in both)
+                pb_progress.Enabled = true;
+                pb_progress.Style = ProgressBarStyle.Marquee;
+                tb_todo.Clear();
+                btn_copy.Enabled = false;
+                sources = LoadDB(tb_source.Text);
+                Dictionary<String, List<Column>> dest = LoadDB(tb_dest.Text);
+                List<String> match = new List<string>();
+                List<String> mismatch = new List<string>();
+
+                var source_only = sources.Keys.Select(t => t).Where(t => !dest.ContainsKey(t));
+                if (source_only.Count() != 0)
                 {
-                    if (source[b].Count() == dest[b].Count()) 
+                    tb_todo.Text += "Source only" + Environment.NewLine;
+                    foreach (var s in source_only)
                     {
-                        if(CompareList(source[b], dest[b]))
+                        tb_todo.Text += "└" + s + Environment.NewLine;
+                    }
+                }
+
+                var both = sources.Select(t => t.Key).Where(t => dest.ContainsKey(t));
+                if (both.Count() != 0)
+                {
+                    foreach (var b in both)
+                    {
+                        if (sources[b].Count() == dest[b].Count())
                         {
-                            match.Add(b);   
+                            if (CompareList(sources[b], dest[b]))
+                            {
+                                match.Add(b);
+                            }
+                            else
+                            {
+                                mismatch.Add(b);
+                            }
                         }
-                        else
+                    }
+
+                    if (match.Count() != 0)
+                    {
+                        tb_todo.Text += "Both" + Environment.NewLine;
+                        foreach (var b in match)
                         {
-                            mismatch.Add(b);
+                            tb_todo.Text += "└" + b + Environment.NewLine;
                         }
                     }
                 }
 
-                if(match.Count() != 0)
+                if (mismatch.Count() != 0)
                 {
-                    tb_todo.Text += "Both" + Environment.NewLine;
-                    foreach (var b in match)
+                    reset();
+                    StringBuilder message = new StringBuilder();
+                    message.Append("The following Tables exist in both databases but contain different columns:");
+                    foreach (var s in mismatch)
                     {
-                        tb_todo.Text += "└" + b + Environment.NewLine;
+                        message.Append(Environment.NewLine);
+                        message.Append(s);
                     }
-                }
-            }
+                    MessageBox.Show(message.ToString());
 
-            if (mismatch.Count() != 0)
-            {
-                StringBuilder message = new StringBuilder();
-                message.Append("The following Tables exist in both databases but contain different columns");
-                foreach (var s in mismatch)
-                {
-                    message.Append(Environment.NewLine);
-                    message.Append(s);
+                    return;
                 }
-                MessageBox.Show(message.ToString());
-                return;
+                else
+                {
+                    sourceOnly = source_only.ToList<String>();
+                    this.both = both.ToList<String>();
+                    btn_copy.Enabled = true;
+                }
             }
-            else
+            catch (Exception ex)
             {
-                sourceOnly = source_only.ToList<String>();
-                this.both = both.ToList<String>();
-                btn_copy.Enabled = true;
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                pb_progress.Style = ProgressBarStyle.Blocks;
+
             }
         }
 
+        /// <summary>
+        /// Compares the list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="l1">The first list</param>
+        /// <param name="l2">The second list</param>
+        /// <returns></returns>
         private bool CompareList<T>(List<T> l1, List<T> l2)
         {
             foreach (var element in l1)
@@ -99,78 +135,70 @@ namespace asql_assign_4
             return true;
         }
 
-        private Dictionary<String, List<Row>> LoadDB(String connectionString)
+        /// <summary>
+        /// Loads the DB.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <returns></returns>
+        private Dictionary<String, List<Column>> LoadDB(String connectionString)
         {
-            Dictionary<String, List<Row>> tableList = new Dictionary<string,List<Row>>();
             SqlConnection db = new SqlConnection(connectionString);
+            Dictionary<String, List<Column>> tableList = new Dictionary<string, List<Column>>();
             db.Open();
-            var tables = db.GetSchema("Tables").AsEnumerable().Select(t => t).OrderBy(t => t["table_name"].ToString().ToLower()).OrderBy(t => t["table_schema"].ToString().ToLower());
-            var columns = db.GetSchema("Columns").AsEnumerable().Select(c => c);
-
+            /*
+             * This assumes they don't want to copy INFORMATION_SCHEMA or sys tables.
+             */
+            var tables = db.GetSchema("Tables").AsEnumerable().Select(t => t).Where(t => !t["table_schema"].Equals("INFORMATION_SCHEMA") && !t["table_schema"].Equals("sys")).OrderBy(t => t["table_name"].ToString().ToLower()).OrderBy(t => t["table_schema"].ToString().ToLower());
+            
             foreach (var t in tables)
             {
                 String table = t["table_schema"] + "." + t["Table_name"];
-                List<Row> rows = new List<Row>();
-                var tableColumns = columns.Where(c => c["table_schema"].Equals(t["table_schema"])).Where(c => c["table_name"].Equals(t["table_name"])).OrderBy(c => c["column_name"].ToString().ToLower());
-
-                foreach (var column in tableColumns)
+                SqlCommand cmd = new SqlCommand(String.Format(@"SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.Columns WHERE TABLE_SCHEMA = '{0}' AND TABLE_NAME = '{1}'", t["table_schema"], t["table_name"]), db);
+                var tableColumns = cmd.ExecuteReader();
+                List<Column> rows = new List<Column>();
+                
+                while(tableColumns.Read())
                 {
-                    rows.Add(new Row(column["column_name"].ToString(), column["data_type"].ToString(), column["character_maximum_length"].ToString()));
+                    rows.Add(new Column(tableColumns["column_name"].ToString(), tableColumns["data_type"].ToString(), tableColumns["character_maximum_length"].ToString()));
                 }
                 tableList.Add(table, rows);
+                tableColumns.Close();
             }
             return tableList;
         }
 
 
-        private class Row
-        {
-            public string name { get; set; }
-            public string type { get; set; }
-            public string size { get; set; }
-
-            public Row(String name, String type, String size)
-            {
-                this.name = name;
-                this.type = type;
-                this.size = size;
-            }
-            
-            public override bool Equals(object obj)
-            {
-                if (obj == null)
-                {
-                    return false;
-                }
-
-                Row p = obj as Row;
-                if ((System.Object)p == null)
-                {
-                    return false;
-                }
-
-                return name.Equals(p.name) && type.Equals(p.type) && size.Equals(p.size);
-            }
-
-            public override int GetHashCode()
-            {
-                return (name + type + size).GetHashCode();
-            }
-        }
-
+        /// <summary>
+        /// Handles the Click event of the btn_copy control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void btn_copy_Click(object sender, EventArgs e)
         {
+            btn_copy.Enabled = false;
             CopyDb(tb_source.Text, tb_dest.Text);
         }
 
+        /// <summary>
+        /// Copies tables from the source to the destination
+        /// </summary>
+        /// <param name="source">The source.</param>
+        /// <param name="destination">The destination.</param>
         private void CopyDb(String source, String destination)
         {
-            
+
             SqlConnection sourceDb = null;
             SqlConnection destDb = null;
             SqlTransaction sourceTrans = null;
             SqlTransaction destTrans = null;
-            try{
+            try
+            {
+                pb_progress.Style = ProgressBarStyle.Blocks;
+                pb_progress.Value = 0;
+                pb_progress.Minimum = 0;
+                pb_progress.Step = 1;
+                pb_progress.Maximum = both.Count + sourceOnly.Count;
+
                 sourceDb = new SqlConnection(source);
                 destDb = new SqlConnection(destination);
                 sourceDb.Open();
@@ -191,11 +219,41 @@ namespace asql_assign_4
                         bulkCopy.WriteToServer(results);
                         results.Close();
                         bulkCopy.Close();
+                        pb_progress.PerformStep();
                     }
                 }
+
+                if (sourceOnly != null && sourceOnly.Count > 0)
+                {
+                    foreach (var table in sourceOnly)
+                    {
+                        StringBuilder createTable = new StringBuilder();
+                        createTable.Append(String.Format(@"create table {0}(", table));
+                        foreach (var row in sources[table])
+                        {
+                            createTable.Append(String.Format(@"{0},", row.toCreateString()));
+                        }
+                        createTable.Length -= 1;
+                        createTable.Append(String.Format(@")"));
+                        SqlCommand cmd = new SqlCommand(createTable.ToString(), destDb, destTrans);
+
+                        cmd.ExecuteNonQuery();
+
+                        cmd = new SqlCommand(String.Format(@"select * from {0}", table), sourceDb);
+                        cmd.Transaction = sourceTrans;
+                        SqlDataReader results = cmd.ExecuteReader();
+
+                        SqlBulkCopy bulkCopy = new SqlBulkCopy(destDb, SqlBulkCopyOptions.Default, destTrans);
+                        bulkCopy.DestinationTableName = table;
+                        bulkCopy.WriteToServer(results);
+                        results.Close();
+                        bulkCopy.Close();
+                        pb_progress.PerformStep();
+                    }
+                }
+
                 sourceTrans.Commit();
                 destTrans.Commit();
-                btn_copy.Enabled = false;
             }
             catch (Exception ex)
             {
@@ -206,16 +264,118 @@ namespace asql_assign_4
 
                 MessageBox.Show(ex.Message);
             }
+            finally
+            {
+                pb_progress.Value = 0;
+            }
         }
 
+        /// <summary>
+        /// Handles the TextChanged event of the tb_source control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void tb_source_TextChanged(object sender, EventArgs e)
         {
-            btn_copy.Enabled = false;
+            reset();
         }
 
+        /// <summary>
+        /// Handles the TextChanged event of the tb_dest control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs" /> instance containing the event data.</param>
         private void tb_dest_TextChanged(object sender, EventArgs e)
         {
+            reset();
+        }
+
+        /// <summary>
+        /// Returns the form to a neutral state
+        /// </summary>
+        private void reset()
+        {
+            tb_todo.Clear();
             btn_copy.Enabled = false;
+            sources.Clear();
+            sourceOnly.Clear();
+            both.Clear();
+            pb_progress.Enabled = false;
+        }
+
+        /// <summary>
+        /// Represents the metadata for a column in a database
+        /// </summary>
+        private class Column
+        {
+            public string name { get; set; }
+            public string type { get; set; }
+            public string size { get; set; }
+
+            public Column(String name, String type, String size)
+            {
+                this.name = name;
+                this.type = type;
+                // I think -1 can be used to signify a few things but this is the only use I've run into
+                if (size.Equals("-1"))
+                {
+                    if (type.Equals("nvarchar"))
+                    {
+                        size = "max";
+                    }
+                    else
+                    {
+                        size = null;
+                    }
+                }
+                this.size = size;
+            }
+
+            /// <summary>
+            /// Determines whether the specified <see cref="System.Object" /> is equal to this instance.
+            /// </summary>
+            /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+            /// <returns>
+            ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+            /// </returns>
+            public override bool Equals(object obj)
+            {
+                if (obj == null)
+                {
+                    return false;
+                }
+
+                Column p = obj as Column;
+                if ((System.Object)p == null)
+                {
+                    return false;
+                }
+
+                return name.Equals(p.name) && type.Equals(p.type) && size.Equals(p.size);
+            }
+
+            /// <summary>
+            /// Returns a hash code for this instance.
+            /// </summary>
+            /// <returns>
+            /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+            /// </returns>
+            public override int GetHashCode()
+            {
+                return (name + type + size).GetHashCode();
+            }
+
+            /// <summary>
+            /// Create a string usable in a table create statement
+            /// </summary>
+            /// <returns>create string</returns>
+            public string toCreateString()
+            {
+                return String.Format(@"[{0}] {1} {2}",
+                    name,
+                    type,
+                    !String.IsNullOrEmpty(size) ? "(" + size + ")" : "");
+            }
         }
     }
 }
